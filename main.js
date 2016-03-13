e10b182f01d346d32b9ef9404fb5cb3cc7170144
Naoki Sekiguchi <naokis@ranagram.com>
2016-03-13 15:17:47 +0900

    Make window background-color transparent

--- main.js
+++ main.js
@@ -21,10 +21,8 @@ app.on("ready", () => {
     resizable: false,
     title: "JSMetronome",
     frame: false,
-    backgroundColor: "transparent",
     transparent: true
   });
-  console.log('__dirname:', __dirname);
   mainWindow.loadURL("file://" + __dirname + "/public/index.html");
   //mainWindow.webContents.openDevTools();
 
@@ -33,26 +31,3 @@ app.on("ready", () => {
   });
 });
 
-
-/*
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-app.on('ready', function() {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
-
-  // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
-});
-*/
